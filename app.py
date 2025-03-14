8c716db3323bafd7ffa8ade12f08a0a8597f6604
Sai2475 <saisanjay3000@gmail.com>
2025-03-15 01:24:19 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import encrypt_decrypt_py  # Import your encryption script
+import encrypt # Import your encryption script
 
 # Streamlit UI
 st.title("🔐 Text Encryption & Decryption App")
@@ -10,7 +10,7 @@ st.header("🔹 Encrypt a Message")
 plain_text = st.text_input("Enter a message to encrypt")
 if st.button("Encrypt"):
     if plain_text:
-        encrypted_text = encrypt_decrypt_py.encrypt(plain_text)  # Use the function from encryption.py
+        encrypted_text = encrypt.encrypt(plain_text)  # Use the function from encryption.py
         st.success(f"🔑 Encrypted Message: {encrypted_text}")
     else:
         st.warning("Please enter text to encrypt.")
@@ -20,7 +20,7 @@ st.header("🔹 Decrypt a Message")
 cipher_text = st.text_input("Enter a message to decrypt")
 if st.button("Decrypt"):
     if cipher_text:
-        decrypted_text = encrypt_decrypt_py.decrypt(cipher_text)  # Use the function from encryption.py
+        decrypted_text = encrypt.decrypt(cipher_text)  # Use the function from encryption.py
         st.success(f"🔓 Original Message: {decrypted_text}")
     else:
         st.warning("Please enter text to decrypt.")

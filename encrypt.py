85c633c6d16b74e7d6360caef7f6e75f4a0dd335
Sai2475 <saisanjay3000@gmail.com>
2025-03-15 00:43:04 +0530

    updated encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,7 +1,13 @@
 import random
 import string
 
-chars = string.ascii_letters + string.digits + string.punctuation
+chars = " " + string.ascii_letters + string.digits + string.punctuation
+chars = list(chars)
 
-print(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+print(f"chars : {chars}")
+print(f"key : {key}")
 
